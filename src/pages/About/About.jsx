1784aae8d57c44b627206eed1e73f141e1fdc208
Jatin-Rai <jatinrai258@gmail.com>
2023-09-12 16:26:05 +0530

    adjusted text size

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -16,14 +16,14 @@ const About = () => {
             exit={{ x: "150%" }}
         >
             <div className="flex flex-col sm:w-1/2 justify-center gap-8">
-                <h1 className="flex flex-col pt-5 gap-4 text-4xl font-semibold">
+                <h1 className="flex flex-col pt-5 gap-4 text-3xl sm:text-4xl font-semibold">
                     Hi,{" "}
                     <span>
                         I'm
                         <span className='text-red-500 dark:text-cyan-300'> Jatin </span>Rai
                     </span>
                 </h1>
-                <div className="text-red-600 dark:text-cyan-300 text-4xl font-bold">
+                <div className="text-red-600 dark:text-cyan-300 text-2xl sm:text-4xl font-bold">
                     <h3>
                         <ReactTyped
                             strings={["Software Engineer", "Frontend Developer", "Techie"]}
@@ -34,7 +34,7 @@ const About = () => {
                         />
                     </h3>
                 </div>
-                <p>
+                <p className='text-sm sm:text-base'>
                     Experienced and creative professional in frontend development with over one year of hands-on experience. Skilled in building engaging user interfaces, problem-solving, feature implementation, and successful deployment.
                 </p>
                 <div className='py-2 animate-bounce'>
@@ -44,18 +44,18 @@ const About = () => {
                         target='_blank'
                         rel="noreferrer"
                     >
-                            View CV
+                        View CV
                     </a>
                 </div>
                 <div className="flex gap-4">
                     <a href="https://www.linkedin.com/in/jatinrai96/" target='_blank' rel="noreferrer">
-                        <FaLinkedin className="text-blue-800 text-3xl hover:animate-bounce" />
+                        <FaLinkedin className="text-blue-800 text-3xl hover:animate-spin" />
                     </a>
                     <a href="https://github.com/Jatin-Rai" target='_blank' rel="noreferrer">
-                        <FaGithub className="text-3xl hover:animate-bounce" />
+                        <FaGithub className="text-3xl hover:animate-spin" />
                     </a>
                     <a href="https://www.instagram.com/jatin.j.rai/" target='_blank' rel="noreferrer">
-                        <FaInstagram className="text-pink-600 text-3xl hover:animate-bounce" />
+                        <FaInstagram className="text-pink-600 text-3xl hover:animate-spin" />
                     </a>
                 </div>
             </div>
